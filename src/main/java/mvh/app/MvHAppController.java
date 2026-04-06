50147718ed0e483e71636255ed759c37fccbf2a4
Yosias Demoz <yosias.demoz@ucalgary.ca>
2026-04-06 01:34:54 -0600

    feat: created a function Load that lets you choose a file then read the world onaction of the load menuitem. then created the print World function that loops over the world and creates a grid and add it to the WorldMap anchorpane.

--- src/main/java/mvh/app/MvHAppController.java
+++ src/main/java/mvh/app/MvHAppController.java
@@ -35,4 +35,61 @@ public class MvHAppController {
     public void initialize() {
     }
 
+    @FXML
+    public void Load() {
+        FileChooser chooser = new FileChooser();
+        chooser.setTitle("Open World File");
+
+        File file = chooser.showOpenDialog(null);
+        if (file == null) return;
+
+        world = MvHReader.loadWorld(file);
+
+        printWorld();
+    }
+
+    @FXML
+    private AnchorPane WorldMap;
+
+    private void printWorld() {
+        WorldMap.getChildren().clear();
+
+        GridPane grid = new GridPane();
+
+
+        int row = world.getRows();
+        int col = world.getColumns();
+        for (int i = 0; i < row + 2; i++) {
+            for (int j = 0; j < col + 2; j++) {
+                String symbol;
+                if (i == 0 || j == 0 || i == row + 1 || j == col + 1) {
+                    symbol = "#";
+                }
+                else {
+                    Object entity = world.getEntity(i - 1, j - 1);
+
+                    if (entity == null) {
+                        symbol = ".";
+                    } else if (entity instanceof Wall) {
+                        symbol = "#";
+                    } else if (entity instanceof Monster && ((Monster) entity).isAlive()) {
+                        symbol = "M";
+                    } else if (entity instanceof Hero && ((Hero) entity).isAlive()) {
+                        symbol = "H";
+                    } else {
+                        symbol = "?";
+                    }
+                }
+                    Label cell = new Label(symbol);
+                    cell.setPrefSize(30, 30);
+                    cell.setAlignment(Pos.CENTER);
+                    cell.setStyle("-fx-border-color: black;");
+
+                    grid.add(cell, j, i);
+
+                }
+            }
+        WorldMap.getChildren().add(grid);
+        }
 }
+
