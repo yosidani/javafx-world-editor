9d79b63dc9183be55e0fbfbb1389f4ea72be6ff6
Yosias Demoz <yosias.demoz@ucalgary.ca>
2026-04-06 21:59:33 -0600

    feat: created the saveWorld function to Write a .txt file from the world

--- src/main/java/mvh/util/MvHWriter.java
+++ src/main/java/mvh/util/MvHWriter.java
@@ -1,4 +1,52 @@
 package mvh.util;
 
+
+import mvh.world.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Class to assist writing a .txt file from the world
+ *
+ * @author Yosias Demoz
+ * @version 1.0
+ */
 public class MvHWriter {
+    /**
+     * Write a .txt file from the world
+     *
+     * @param file  The file to write to
+     * @param world The world to write from(read)
+     */
+    public static void saveWorld(File file, World world) {
+        try (PrintWriter writer = new PrintWriter(file)) {
+            writer.println(world.getRows());
+            writer.println(world.getColumns());
+
+            for (int i = 0; i < world.getRows(); i++) {
+                for (int j = 0; j < world.getColumns(); j++) {
+                    StringBuilder line = new StringBuilder();
+                    line.append(i).append(",").append(j);
+                    Entity entity = world.getEntity(i, j);
+                    if (entity != null) {
+                        if(entity instanceof Wall) {
+                            line.append(",WALL");
+
+                        } else if (entity instanceof Hero){
+                            line.append(",HERO").append(",").append(entity.getSymbol()).append(",").append(entity.getHealth()).append(",").append(entity.weaponStrength()).append(",").append(entity.armourStrength());
+
+                        } else if (entity instanceof Monster) {
+                            Monster mon = (Monster) entity;
+                            line.append(",MONSTER").append(",").append(mon.getSymbol()).append(",").append(mon.getHealth()).append(",").append(mon.getWeaponType());
+                        }
+                    }
+                    writer.println(line.toString());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
