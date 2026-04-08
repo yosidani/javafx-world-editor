613af130547c3cd91eda89a083050a47251b8388
Yosias Demoz <yosias.demoz@ucalgary.ca>
2026-04-07 20:18:11 -0600

    feat: made a feature to adjust(hide) what it shows based on the type it recives from the mvhappcontroller

--- src/main/java/mvh/app/AddController.java
+++ src/main/java/mvh/app/AddController.java
@@ -1,4 +1,46 @@
 package mvh.app;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import mvh.world.World;
+import mvh.app.MvHAppController.AddType;
+
+import java.io.IOException;
+import java.util.List;
+
 public class AddController {
+
+    @FXML
+    private Label titleLabel;
+    @FXML
+    private VBox weaponBox;
+    @FXML
+    private VBox heroBox;
+    @FXML
+    private Label statusLabel;
+
+    public void addEntity(AddType type) {
+        if (type == AddType.HERO) {
+            titleLabel.setText("Hero");
+            heroBox.setVisible(true);
+            heroBox.setManaged(true);
+            weaponBox.setVisible(false);
+            weaponBox.setManaged(false);
+
+            statusLabel.setText("Add a Hero.");
+        } else {
+            titleLabel.setText("Monster");
+            weaponBox.setVisible(true);
+            weaponBox.setManaged(true);
+            heroBox.setVisible(false);
+            heroBox.setManaged(false);
+            statusLabel.setText("Add a Monster.");
+        }
+    }
 }
