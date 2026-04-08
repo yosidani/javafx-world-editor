3b823f7b0c2e1725a92cf100c3767047c9a8d700
Yosias Demoz <yosias.demoz@ucalgary.ca>
2026-04-08 02:22:52 -0600

    feat: created finctions to get the symbol, health, wepon etc of entities from the pop up stage input to transfer to MvHAppController.

--- src/main/java/mvh/app/AddController.java
+++ src/main/java/mvh/app/AddController.java
@@ -4,7 +4,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -24,6 +26,16 @@ public class AddController {
     private VBox heroBox;
     @FXML
     private Label statusLabel;
+    @FXML
+    private TextField symbolField;
+    @FXML
+    private TextField healthField;
+    @FXML
+    private TextField attackField;
+    @FXML
+    private TextField armorField;
+    @FXML
+    private ComboBox<String> weaponComboBox;
 
     public void addEntity(AddType type) {
         if (type == AddType.HERO) {
@@ -32,8 +44,8 @@ public class AddController {
             heroBox.setManaged(true);
             weaponBox.setVisible(false);
             weaponBox.setManaged(false);
-
             statusLabel.setText("Add a Hero.");
+            statusLabel.setTextFill(Color.BLACK);
         } else {
             titleLabel.setText("Monster");
             weaponBox.setVisible(true);
@@ -41,6 +53,75 @@ public class AddController {
             heroBox.setVisible(false);
             heroBox.setManaged(false);
             statusLabel.setText("Add a Monster.");
+            statusLabel.setTextFill(Color.BLACK);
         }
+    }
+
+    public char getSymbol() {
+        if  (symbolField.getText().equals("")) {
+            statusLabel.setText("Please enter a symbol.");
+            symbolField.setStyle("-fx-border-color: red; -fx-border-width: 2px; -fx-border-radius: 3px;");
+            statusLabel.setTextFill(Color.RED);
+            return '?';
+        }
+        statusLabel.setText("Continue adding.");
+        statusLabel.setTextFill(Color.BLACK);
+        symbolField.setStyle("");
+        return symbolField.getText().charAt(0);
+    }
+    public String getHealth() {
+        try {
+            Integer.parseInt(healthField.getText());
+            statusLabel.setText("Continue adding.");
+            statusLabel.setTextFill(Color.BLACK);
+            healthField.setStyle("");
+            return healthField.getText();
+        } catch (NumberFormatException e) {
+            statusLabel.setText("Please enter valid health.");
+            statusLabel.setTextFill(Color.RED);
+            healthField.setStyle("-fx-border-color: red; -fx-border-width: 2px; -fx-border-radius: 3px;");
+            return "?";
+        }
+    }
+
+    public String getHeroAttack() {
+        try {
+            Integer.parseInt(attackField.getText());
+            statusLabel.setText("Continue adding a hero.");
+            statusLabel.setTextFill(Color.BLACK);
+            attackField.setStyle("");
+            return attackField.getText();
+        } catch (NumberFormatException e) {
+            statusLabel.setText("Please enter valid attack strength.");
+            statusLabel.setTextFill(Color.RED);
+            attackField.setStyle("-fx-border-color: red; -fx-border-width: 2px; -fx-border-radius: 3px;");
+            return "?";
+        }
+    }
+    public String getHeroArmor() {
+        try {
+            Integer.parseInt(armorField.getText());
+            statusLabel.setText("Continue adding a hero.");
+            statusLabel.setTextFill(Color.BLACK);
+            armorField.setStyle("");
+            return armorField.getText();
+        } catch (NumberFormatException e) {
+            statusLabel.setText("Please enter valid armor strength.");
+            statusLabel.setTextFill(Color.RED);
+            armorField.setStyle("-fx-border-color: red; -fx-border-width: 2px; -fx-border-radius: 3px;");
+            return "?";
+        }
+    }
+    public char getMonsterWeapon() {
+        if (weaponComboBox.getValue() == null) {
+            statusLabel.setText("Please enter a weapon.");
+            weaponComboBox.setStyle("-fx-border-color: red; -fx-border-width: 2px; -fx-border-radius: 3px;");
+            statusLabel.setTextFill(Color.RED);
+            return '?';
+        }
+        statusLabel.setText("Continue adding a monster.");
+        statusLabel.setTextFill(Color.BLACK);
+        weaponComboBox.setStyle("");
+        return weaponComboBox.getValue().charAt(0);
     }
 }
